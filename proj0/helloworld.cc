c4be40938e4d987ea3731ba993742c5f7ee2eb5c
Mark <markamay@live.com>
2018-12-06 15:26:44 -0500

    Initial Commit

---
+++ proj0/helloworld.cc
@@ -0,0 +1,15 @@
+/*
+  Author: Mark May
+  Description: This is just a test of GitHub.
+  Date: 12/06/2018
+*/
+
+#include <iostream>
+
+int main() {
+
+  std::cout << "Hello World, I am Mark! :)" << std::endl;
+
+  return 0;
+
+}
